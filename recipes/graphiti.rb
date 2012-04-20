d04391f786bf14810ff0ae8012e27c5560b41918
Jason Rust <jason@rustedcode.com>
2012-04-20 16:22:34 -0700

    Adding graphiti

--- recipes/graphiti.rb
+++ recipes/graphiti.rb
@@ -6,16 +6,6 @@ execute "checkout graphiti" do
   cwd "/var/www"
 end
 
-execute "graphiti-bundler" do
-  cwd "/var/www/graphiti.lessonplanet.com"
-  user "root"
-  command <<-EOH
-  chown -R deploy:deploy *
-  rvm_path=/usr/local/rvm /usr/local/bin/rvm-shell '1.9.3@global' -c 'bundle install --deployment --without development test'
-  rvm_path=/usr/local/rvm /usr/local/bin/rvm-shell '1.9.3@global' -c 'bundle exec rake graphiti:metrics'
-  EOH
-end
-
 s3_creds = Chef::EncryptedDataBagItem.load('passwords', 'amazon_s3')
 template "/var/www/graphiti.lessonplanet.com/config/amazon_s3.yml" do
   owner "deploy"
@@ -28,6 +18,16 @@ template "/var/www/graphiti.lessonplanet.com/config/settings.yml" do
   owner "deploy"
   mode "0600"
   source "settings.yml.erb"
+end
+
+execute "graphiti-bundler" do
+  cwd "/var/www/graphiti.lessonplanet.com"
+  user "root"
+  command <<-EOH
+  chown -R deploy:deploy *
+  rvm_path=/usr/local/rvm /usr/local/bin/rvm-shell '1.9.3@global' -c 'bundle install --deployment --without development test'
+  rvm_path=/usr/local/rvm /usr/local/bin/rvm-shell '1.9.3@global' -c 'bundle exec rake graphiti:metrics'
+  EOH
 end
 
 web_app 'graphiti' do
